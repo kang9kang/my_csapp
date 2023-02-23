c423285b3eaade78ad608c0f7a77683c22cdb561
kang9kang <13589130504@163.com>
2023-02-23 13:12:23 +0800

    finished first bomb

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -344,13 +344,13 @@ Disassembly of section .text:
   400edf:	90                   	nop
 
 0000000000400ee0 <phase_1>:
-  400ee0:	48 83 ec 08          	sub    $0x8,%rsp
-  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi
-  400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal>
-  400eee:	85 c0                	test   %eax,%eax
-  400ef0:	74 05                	je     400ef7 <phase_1+0x17>
-  400ef2:	e8 43 05 00 00       	callq  40143a <explode_bomb>
-  400ef7:	48 83 c4 08          	add    $0x8,%rsp
+  400ee0:	48 83 ec 08          	sub    $0x8,%rsp ;rdi = input 的地址
+  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi ;esi = ans 的地址
+  400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal> ;比较两个字符串是否相等
+  400eee:	85 c0                	test   %eax,%eax ;如果相等，eax = 0，否则eax = 1
+  400ef0:	74 05                	je     400ef7 <phase_1+0x17> ;如果相等，跳转到 400ef7
+  400ef2:	e8 43 05 00 00       	callq  40143a <explode_bomb> ;如果不相等，爆炸
+  400ef7:	48 83 c4 08          	add    $0x8,%rsp ;恢复栈
   400efb:	c3                   	retq   
 
 0000000000400efc <phase_2>:
@@ -702,37 +702,37 @@ Disassembly of section .text:
   401338:	41 54                	push   %r12
   40133a:	55                   	push   %rbp
   40133b:	53                   	push   %rbx
-  40133c:	48 89 fb             	mov    %rdi,%rbx
-  40133f:	48 89 f5             	mov    %rsi,%rbp
+  40133c:	48 89 fb             	mov    %rdi,%rbx ;rbx = input 的地址
+  40133f:	48 89 f5             	mov    %rsi,%rbp ;rbp = ans 的地址
   401342:	e8 d4 ff ff ff       	callq  40131b <string_length>
-  401347:	41 89 c4             	mov    %eax,%r12d
-  40134a:	48 89 ef             	mov    %rbp,%rdi
+  401347:	41 89 c4             	mov    %eax,%r12d ;r12d = input 的长度
+  40134a:	48 89 ef             	mov    %rbp,%rdi  ;rdi = ans 的地址
   40134d:	e8 c9 ff ff ff       	callq  40131b <string_length>
-  401352:	ba 01 00 00 00       	mov    $0x1,%edx
-  401357:	41 39 c4             	cmp    %eax,%r12d
-  40135a:	75 3f                	jne    40139b <strings_not_equal+0x63>
-  40135c:	0f b6 03             	movzbl (%rbx),%eax
-  40135f:	84 c0                	test   %al,%al
-  401361:	74 25                	je     401388 <strings_not_equal+0x50>
-  401363:	3a 45 00             	cmp    0x0(%rbp),%al
-  401366:	74 0a                	je     401372 <strings_not_equal+0x3a>
+  401352:	ba 01 00 00 00       	mov    $0x1,%edx ;暂时标记为不相等
+  401357:	41 39 c4             	cmp    %eax,%r12d ;比较input和ans的长度是否相等
+  40135a:	75 3f                	jne    40139b <strings_not_equal+0x63> ;不相等就跳转到40139b，返回为1
+  40135c:	0f b6 03             	movzbl (%rbx),%eax ;取出input的第一个字符
+  40135f:	84 c0                	test   %al,%al ;判断是否为0
+  401361:	74 25                	je     401388 <strings_not_equal+0x50> ;如果是0，就跳转到401388，返回为0
+  401363:	3a 45 00             	cmp    0x0(%rbp),%al ;比较input的第一个字符和ans的第一个字符是否相等
+  401366:	74 0a                	je     401372 <strings_not_equal+0x3a> ;如果相等，就跳转到401372，继续比较下一个字符
   401368:	eb 25                	jmp    40138f <strings_not_equal+0x57>
-  40136a:	3a 45 00             	cmp    0x0(%rbp),%al
-  40136d:	0f 1f 00             	nopl   (%rax)
-  401370:	75 24                	jne    401396 <strings_not_equal+0x5e>
-  401372:	48 83 c3 01          	add    $0x1,%rbx
-  401376:	48 83 c5 01          	add    $0x1,%rbp
-  40137a:	0f b6 03             	movzbl (%rbx),%eax
-  40137d:	84 c0                	test   %al,%al
-  40137f:	75 e9                	jne    40136a <strings_not_equal+0x32>
-  401381:	ba 00 00 00 00       	mov    $0x0,%edx
-  401386:	eb 13                	jmp    40139b <strings_not_equal+0x63>
-  401388:	ba 00 00 00 00       	mov    $0x0,%edx
-  40138d:	eb 0c                	jmp    40139b <strings_not_equal+0x63>
-  40138f:	ba 01 00 00 00       	mov    $0x1,%edx
-  401394:	eb 05                	jmp    40139b <strings_not_equal+0x63>
-  401396:	ba 01 00 00 00       	mov    $0x1,%edx
-  40139b:	89 d0                	mov    %edx,%eax
+  40136a:	3a 45 00             	cmp    0x0(%rbp),%al ;比较input的下一个字符和ans的下一个字符是否相等
+  40136d:	0f 1f 00             	nopl   (%rax) ;nop
+  401370:	75 24                	jne    401396 <strings_not_equal+0x5e> ;如果不相等，就跳转到401396，返回为1
+  401372:	48 83 c3 01          	add    $0x1,%rbx ;如果相等，就比较下一个字符
+  401376:	48 83 c5 01          	add    $0x1,%rbp ;如果相等，就比较下一个字符
+  40137a:	0f b6 03             	movzbl (%rbx),%eax ;取出input的下一个字符
+  40137d:	84 c0                	test   %al,%al ;判断是否为0
+  40137f:	75 e9                	jne    40136a <strings_not_equal+0x32> ;如果不为0，就跳转到40136a，继续比较下一个字符
+  401381:	ba 00 00 00 00       	mov    $0x0,%edx ;如果input的第一个字符为0，就跳转到401386，返回为0
+  401386:	eb 13                	jmp    40139b <strings_not_equal+0x63> 
+  401388:	ba 00 00 00 00       	mov    $0x0,%edx ;如果input的第一个字符为0，就跳转到401386，返回为0
+  40138d:	eb 0c                	jmp    40139b <strings_not_equal+0x63> 
+  40138f:	ba 01 00 00 00       	mov    $0x1,%edx ;如果input的第一个字符不为0，就跳转到401396，返回为1
+  401394:	eb 05                	jmp    40139b <strings_not_equal+0x63> 
+  401396:	ba 01 00 00 00       	mov    $0x1,%edx ;如果input的第一个字符不为0，就跳转到401396，返回为1
+  40139b:	89 d0                	mov    %edx,%eax ;返回值
   40139d:	5b                   	pop    %rbx
   40139e:	5d                   	pop    %rbp
   40139f:	41 5c                	pop    %r12
