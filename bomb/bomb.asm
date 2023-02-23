fe512ce9ca0985c0bd542805e7283638ce55635a
kang9kang <13589130504@163.com>
2023-02-23 16:01:24 +0800

    finished fourth bomb

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -418,50 +418,50 @@ Disassembly of section .text:
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
 
-0000000000400fce <func4>:
+0000000000400fce <func4>: 
   400fce:	48 83 ec 08          	sub    $0x8,%rsp
-  400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
-  400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
-  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
-  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
+  400fd2:	89 d0                	mov    %edx,%eax ;eax = edx
+  400fd4:	29 f0                	sub    %esi,%eax ;eax = eax - esi
+  400fd6:	89 c1                	mov    %eax,%ecx ;ecx = eax
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx ;ecx = eax >> 31
+  400fdb:	01 c8                	add    %ecx,%eax ;eax = eax + ecx
+  400fdd:	d1 f8                	sar    %eax ;eax = (edx - esi) / 2
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx ;ecx = rax + rsi * 1
+  400fe2:	39 f9                	cmp    %edi,%ecx ;edi == (ecx = (edx + esi) / 2)
+  400fe4:	7e 0c                	jle    400ff2 <func4+0x24> ;edi <= ecx ,跳转
+  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx ;edx = rcx - 1
   400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
-  400fee:	01 c0                	add    %eax,%eax
+  400fee:	01 c0                	add    %eax,%eax ;eax = eax * 2
   400ff0:	eb 15                	jmp    401007 <func4+0x39>
   400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
   400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
+  400ff9:	7d 0c                	jge    401007 <func4+0x39> ;edi >= ecx ,跳转出函数
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi ;esi = rcx + 1
   400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
-  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
+  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax 
   401007:	48 83 c4 08          	add    $0x8,%rsp
   40100b:	c3                   	retq   
 
-000000000040100c <phase_4>:
+000000000040100c <phase_4>: ;输入的字符串必须是 "0 0" or "1 0" or "3 0" or "7 0"，否则爆炸
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
   401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
   401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
   40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
   40101f:	b8 00 00 00 00       	mov    $0x0,%eax
   401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
+  401029:	83 f8 02             	cmp    $0x2,%eax ;读取两个数字
   40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp) ;
   401033:	76 05                	jbe    40103a <phase_4+0x2e>
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
   40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
   40103f:	be 00 00 00 00       	mov    $0x0,%esi
   401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
   401048:	e8 81 ff ff ff       	callq  400fce <func4>
-  40104d:	85 c0                	test   %eax,%eax
-  40104f:	75 07                	jne    401058 <phase_4+0x4c>
-  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
-  401056:	74 05                	je     40105d <phase_4+0x51>
+  40104d:	85 c0                	test   %eax,%eax ;判断返回值是否为0
+  40104f:	75 07                	jne    401058 <phase_4+0x4c> ;不为0则爆炸
+  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp) ;判断第二个数字是否为0
+  401056:	74 05                	je     40105d <phase_4+0x51> ;为0则跳转
   401058:	e8 dd 03 00 00       	callq  40143a <explode_bomb>
   40105d:	48 83 c4 18          	add    $0x18,%rsp
   401061:	c3                   	retq   
